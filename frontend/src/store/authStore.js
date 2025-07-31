f592887e70969268dbaa8e7fa816beac44192a1e
abylsliam44 <abylajslamzanov@gmail.com>
2025-07-31 14:04:36 +0500

    Fix import path for api.js in authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import Cookies from 'js-cookie'
-import { authAPI } from '../lib/api'
+import { authAPI } from '../lib/api.js'
 
 const useAuthStore = create(
   persist(
